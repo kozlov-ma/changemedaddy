7c21503ceeee79776f7b25a88ab44b45ae6b1573
Mikhail Kozlov <wmikkoz@icloud.com>
2024-04-05 21:54:53 +0500

    charm.sh logger

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	"changemedaddy/api"
 	"changemedaddy/db/inmem"
 	"changemedaddy/market/fake"
+	"github.com/charmbracelet/log"
 	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 	db := inmem.New()
-	log := slog.Default()
+
+	handler := log.New(os.Stderr)
+	log := slog.New(handler)
+
 	market := fake.NewMarket()
 	api := api.New(db, market, log)
 
