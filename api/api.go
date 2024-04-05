73bfc7c762bf199b4eda6dbc9252ddf8bfca9ea2
Mikhail Kozlov <wmikkoz@icloud.com>
2024-04-05 15:26:30 +0500

    Add useful middleware

--- api/api.go
+++ api/api.go
@@ -3,9 +3,11 @@ package api
 import (
 	"changemedaddy/invest"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 	slogchi "github.com/samber/slog-chi"
 	"log/slog"
+	"time"
 )
 
 type DB interface {
@@ -28,12 +30,17 @@ func (api API) NewRouter() chi.Router {
 	r := chi.NewRouter()
 
 	logConfig := slogchi.Config{
-		WithSpanID:  true,
-		WithTraceID: true,
+		WithRequestID: true,
+		WithSpanID:    true,
+		WithTraceID:   true,
 	}
 
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
 	r.Use(slogchi.NewWithConfig(api.log, logConfig))
+	r.Use(middleware.Timeout(4 * time.Second))
+	r.Use(middleware.Heartbeat("ping"))
 
 	r.Route("/api", api.router)
 
