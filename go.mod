2727b302c85606e759418a58b68c5bb14f663de7
Mikhail Kozlov <wmikkoz@icloud.com>
2024-04-09 22:08:44 +0500

    fx for server

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-chi/render v1.0.3
 	github.com/go-playground/validator/v10 v10.19.0
 	github.com/samber/slog-chi v1.9.1
+	go.uber.org/fx v1.21.0
 )
 
 require (
@@ -28,6 +29,9 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	go.opentelemetry.io/otel v1.19.0 // indirect
 	go.opentelemetry.io/otel/trace v1.19.0 // indirect
+	go.uber.org/dig v1.17.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.21.0 // indirect
---
+++ server/server.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"context"
+	"github.com/charmbracelet/log"
+	"github.com/go-chi/chi/v5"
+	"go.uber.org/fx"
+	"log/slog"
+	"net"
+	"net/http"
+)
+
+type Router interface {
+	http.Handler
+	Pattern() string
+}
+
+func NewServerMux(routes []Router) *chi.Mux {
+	r := chi.NewRouter()
+
+	for _, route := range routes {
+		r.Mount(route.Pattern(), route)
+	}
+
+	return r
+}
+
+func New(mux *chi.Mux, log *slog.Logger) *http.Server {
+	log = log.With("module", "server")
+	srv := &http.Server{Addr: ":80", Handler: mux}
+	return srv
+}
+
+func Run(lc fx.Lifecycle, srv *http.Server) {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+
+			log.Info("Starting HTTP server", "addr", srv.Addr)
+			go srv.Serve(ln)
+
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			log.Info("Shutting down HTTP server", "addr", srv.Addr)
+			return srv.Shutdown(ctx)
+		}})
+}
