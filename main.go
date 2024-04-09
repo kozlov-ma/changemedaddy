29664c31ebdb6435cc71720b502f5fc79eb34ad7
Mikhail Kozlov <wmikkoz@icloud.com>
2024-04-09 22:08:49 +0500

    fx for app

--- main.go
+++ main.go
@@ -4,18 +4,78 @@ import (
 	"changemedaddy/db/inmem"
 	"changemedaddy/market/fake"
 	"changemedaddy/server"
-	"github.com/charmbracelet/log"
+	"changemedaddy/server/api"
+	"changemedaddy/server/core"
+	"changemedaddy/server/web"
+	charmlog "github.com/charmbracelet/log"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 	"log/slog"
 	"os"
 )
 
+func appLogger() *slog.Logger {
+	handler := charmlog.New(os.Stderr)
+	return slog.New(handler)
+}
+
+func asRouter(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(server.Router)),
+		fx.ResultTags(`group:"routers"`),
+	)
+}
+
+func asDB(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(core.DB)),
+	)
+}
+
+func asMarketProvider(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(core.MarketProvider)),
+	)
+}
+
 func main() {
-	db := inmem.New()
+	fx.New(
+		// Add logger
+		fx.Provide(appLogger),
+		fx.WithLogger(func(l *slog.Logger) fxevent.Logger {
+			return &fxevent.SlogLogger{Logger: l}
+		}),
 
-	handler := log.New(os.Stderr)
-	log := slog.New(handler)
+		// Create database and market services
+		fx.Provide(asDB(inmem.New)),
+		fx.Provide(asMarketProvider(fake.NewMarket)),
 
-	market := fake.NewMarket()
+		// Create routers for server
+		fx.Provide(
+			asRouter(api.New),
+		),
+		fx.Provide(
+			asRouter(web.New),
+		),
 
-	server.Run(market, db, log)
+		// Create routing mux for server
+		fx.Provide(
+			fx.Annotate(
+				server.NewServerMux,
+				fx.ParamTags(`group:"routers"`),
+			),
+		),
+		// Create server
+		fx.Provide(
+			server.New,
+		),
+
+		// Start server
+		fx.Invoke(
+			server.Run,
+		),
+	).Run()
 }
