b64e1c6dae67345fe4a947477e64a324eda46988
Mikhail Kozlov <wmikkoz@icloud.com>
2024-04-23 21:21:33 +0500

    app errors

--- internal/pkg/apperr/error.go
+++ internal/pkg/apperr/error.go
@@ -6,10 +6,16 @@ import (
 	"github.com/go-chi/render"
 )
 
+type Fix struct {
+	name  string
+	value any
+}
+
 type Error struct {
-	Err      error   `json:"-"`
-	AppCode  AppCode `json:"app_code"`
-	HTTPCode int     `json:"-"`
+	Err      error  `json:"-"`
+	AppCode  string `json:"app_code"`
+	HTTPCode int    `json:"-"`
+	Fixes    []Fix  `json:"fixes"`
 }
 
 func (e *Error) Render(r *http.Request) error {
