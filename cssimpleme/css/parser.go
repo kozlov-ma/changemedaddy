d6f5db18ef531ff77ec795f4cc8ba36042927ade
L0veR1ck <lavr.rar@yandex.ru>
2024-06-12 18:02:39 +0500

    parser.work

--- cssimpleme/css/parser.go
+++ cssimpleme/css/parser.go
@@ -1,6 +1,9 @@
 package css
 
-import "cssimpleme/ast"
+import (
+	"cssimpleme/ast"
+	"strings"
+)
 
 type ClassRegistry interface {
 	Find(name string) (cls *Class, ok bool)
@@ -22,5 +25,40 @@ type Parser struct {
 }
 
 func (p *Parser) Work() {
-	panic("implement me")
+	variantsClassValue := <-p.Input
+
+	possibleVariants := strings.Split(variantsClassValue, ":")
+	numVariants := len(possibleVariants)
+
+	if numVariants == 0 {
+		return
+	}
+
+	variants := make([]ApplyVariant, 0, numVariants-1)
+	for _, v := range possibleVariants[:numVariants-1] {
+		if vr, ok := p.va.Find(v); ok {
+			variants = append(variants, vr)
+		} else {
+			p.UnknownVariants <- v
+		}
+	}
+
+	classValue := possibleVariants[numVariants-1]
+	var class *Class
+	var value string
+
+	for end := len(classValue); end > 0; end-- {
+		if cl, ok := p.cls.Find(classValue[:end]); ok {
+			class = cl
+			value = classValue[end:]
+			break
+		}
+	}
+
+	parsed := parsed{
+		Class:    class,
+		Value:    value,
+		Variants: variants,
+	}
+	p.Output <- parsed.ToRule()
 }
