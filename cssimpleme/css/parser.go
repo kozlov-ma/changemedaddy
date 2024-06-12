3ad3928d19084e7e5d0f595026ba0c5043f01a51
Mikhail Kozlov <wmikkoz@icloud.com>
2024-06-12 17:11:37 +0500

    css

--- cssimpleme/css/parser.go
+++ cssimpleme/css/parser.go
@@ -2,7 +2,18 @@ package css
 
 import "cssimpleme/ast"
 
+type ClassRegistry interface {
+	Find(name string) (cls *Class, ok bool)
+}
+
+type VariantRegistry interface {
+	Find(name string) (av ApplyVariant, ok bool)
+}
+
 type Parser struct {
+	cls ClassRegistry
+	va  VariantRegistry
+
 	Input  <-chan string
 	Output chan<- *ast.Rule
 
